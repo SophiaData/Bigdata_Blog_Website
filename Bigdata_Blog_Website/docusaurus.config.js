9fe18a3f3539f96ce4a62f662d227824fd590298
rookiegao <rookiegao712@gmail>
2022-11-17 22:30:06 +0800

    [blog] test blog

--- Bigdata_Blog_Website/docusaurus.config.js
+++ Bigdata_Blog_Website/docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'sophiadata', // Usually your GitHub org/user name.
-  projectName: 'bigdata-blog', // Usually your repo name.
+  organizationName: 'SophiaData', // Usually your GitHub org/user name.
+  projectName: 'Bigdata_Blog_Website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
